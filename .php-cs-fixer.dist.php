1a2081f291b59c23a4b2c286e80240fa59ddc5e1
Matt Harvey <software@matthewharvey.net>
2026-09-10 11:17:05 +0000

    Add PHP CS Fixer and PHPStan tooling

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->in(__DIR__ . '/bin')
+    ->in(__DIR__ . '/app');
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS2.0' => true,
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
